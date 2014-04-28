809750c597b1a74254d679733f7f602db071b2ab
Mark Funk <mark@boomtownroi.com>
2014-04-28 15:22:23 -0400

    Updates

---
+++ angularapi/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "boomstrap": "boomstrap"
+  },
+  "pages": [
+    {
+      "section": "boomstrap",
+      "id": "boomstrap.directive:btAddClassOnLoad",
+      "shortName": "btAddClassOnLoad",
+      "type": "directive",
+      "moduleName": "boomstrap",
+      "shortDescription": "The btAddClassOnLoad directive adds a class to an img tag",
+      "keywords": "add adds bob boomstrap bt-add-class-on-load btaddclassonload class css directive event fires image img jpg load loaded my-img representation src string tag transitions"
+    }
+  ],
+  "apis": {
+    "boomstrap": false
+  },
+  "html5Mode": false,
+  "startPage": "/boomstrap",
+  "scripts": [
+    "boomstrap.js",
+    "angular-animate.min.js"
+  ]
+};
