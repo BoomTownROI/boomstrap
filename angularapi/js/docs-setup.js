a6986e09cd288692e8261a4020a25a4a6f3f3d3c
Mark Funk <mark@boomtownroi.com>
2014-04-28 17:31:26 -0400

    Updates

--- angularapi/js/docs-setup.js
+++ angularapi/js/docs-setup.js
@@ -20,6 +20,15 @@ NG_DOCS={
       "moduleName": "boomstrap",
       "shortDescription": "The btAffix element allows a user to affix an element at a given scroll point.",
       "keywords": "$window add affix affixed affixing allowed allows boomstrap break bt-affix btaffix directive element ensure entire fullheight height images indicates jpg layout left module ngboomstrap number offset pinnedheader pixels place point scroll spans src top user width"
+    },
+    {
+      "section": "ngboomstrap",
+      "id": "boomstrap.directive:btAutoSubmit",
+      "shortName": "btAutoSubmit",
+      "type": "directive",
+      "moduleName": "boomstrap",
+      "shortDescription": "btAutoSubmit allows you to auto submit a form whenever the model of a form-field changes.",
+      "keywords": "$scope $timeout allows angular auto autosubmittest boomstrap bt-auto-submit btautosubmit change changes completion controller delayedvalue directive element expression false fire fired fires firing form form-field function inputvalue milliseconds model module myname ng-app ng-bind ng-controller ng-model ng-show ng-submit ngboomstrap ngform ngsubmit number requires scheduled set submit submit-timeout submittimeout subtest supplied thinking timeout true truthy type wait will-load willload"
     }
   ],
   "apis": {
