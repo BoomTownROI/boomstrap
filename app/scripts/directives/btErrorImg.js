f4e7e8754ce0714d892624b6730b7190ae6290c5
Mark Funk <mark@boomtownroi.com>
2014-04-30 16:33:05 -0400

    Commenting btErrorImg

--- app/scripts/directives/btErrorImg.js
+++ app/scripts/directives/btErrorImg.js
@@ -1,5 +1,27 @@
 (function(Boomstrap) {
   'use strict';
+
+  /**
+   * @ngdoc directive
+   * @name  boomstrap.directive:btErrorImg
+   * @restrict A
+   * 
+   * @param {string} btErrorImg URL to a backup image to be used if the src of an img tag fails to load.
+   *
+   * @description
+   * The `btErrorImg` directive will replace the src of an img tag if the original src fails to load.
+   * This is useful for images of properties.
+   *
+   * @element img
+   *
+   * @example
+      <doc:example module="boomstrap">
+        <doc:source>
+          <img src="images/i-do-not-exist.jpg" bt-error-img="images/fpo-he-man.jpg"/>
+        </doc:source>
+      </doc:example>
+   * 
+   */
   Boomstrap.directive('btErrorImg', function() {
     return {
       link: function(scope, element, attrs) {
