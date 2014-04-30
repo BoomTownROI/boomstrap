f2311955ec2e86f5ccdc45a08b3049441e3402d1
Sean McCambridge <sean@boomtownroi.com>
2014-04-30 14:07:42 -0400

    price history and sash logic

--- app/scripts/controllers/PropertyCardDemoCtrl.js
+++ app/scripts/controllers/PropertyCardDemoCtrl.js
@@ -8,11 +8,20 @@
     $scope.property.defaultImageSrc = 'http://placekitten.com/800/600';
     $scope.property.listPrice = '$2,250,000';
     $scope.property.listingUrl = 'https://www.google.com/search?q=quaint+shack&tbm=isch';
-    $scope.property.offMarket = false;
+    $scope.property.offMarket = false; // only populated for off market listings (triggers sash)
+    $scope.property.newProperty = '6 hours ago'; // only populated for new listings (triggers sash)
+    $scope.property.reduced = false; // only populated for reductions (triggers sash)
+    // $scope.property.reduced = {
+    //     'when': '5 hrs ago',
+    //     'change': '-$250,000',
+    //     'changePercent': '2%',
+    //     'price': '$2,250,000'
+    // }
+    $scope.property.backOnMarket = false; // only populated for back on market (triggers sash)
     $scope.property.bestFits = '1444';
     $scope.property.favs = '33';
     $scope.property.mls = '1324961';
-    $scope.property.listed = '6 days ago';
+    $scope.property.listed = '6 months ago';
     $scope.property.pricePerSqft = '$1.99';
     $scope.property.beds = '5';
     $scope.property.baths = '3.5';
@@ -25,5 +34,20 @@
     $scope.property.address.city = 'Charleston';
     $scope.property.address.state = 'SC';
     $scope.property.address.neighborhood = 'Craig\'s Landing';
+
+    $scope.property.history = [
+        {
+            'when': '5 hrs ago',
+            'change': '-$250,000',
+            'changePercent': '2%',
+            'price': '$2,250,000'
+        },
+        {
+            'when': '6 weeks ago',
+            'change': '-$500,000',
+            'changePercent': '5%',
+            'price': '$2,500,000'
+        }
+    ];
   }]);
 })(angular.module('boomstrap'));
