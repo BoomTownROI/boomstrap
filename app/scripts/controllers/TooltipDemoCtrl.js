c76c28d219f894df70fa1a48013540013534e3ba
Sean McCambridge <sean@boomtownroi.com>
2014-05-01 14:50:42 -0400

    cleaned up unused controllers which have since moved to codepen

--- app/scripts/controllers/TooltipDemoCtrl.js
+++
@@ -1,8 +0,0 @@
-(function(boomstrap) {
-  'use strict';
-  boomstrap.controller('TooltipDemoCtrl', ['$scope', function TooltipDemoCtrl($scope) {
-    $scope.dynamicTooltip = 'Hello, World!';
-    $scope.dynamicTooltipText = 'dynamic';
-    $scope.htmlTooltip = 'I&#x27;ve been made &lt;b&gt;bold&lt;/b&gt;!';
-  }]);
-})(angular.module('boomstrap'));
