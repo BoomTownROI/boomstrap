518b0f065bc5333fc37040bf333f09e724f31fed
Mark Funk <mark@boomtownroi.com>
2014-06-10 15:42:56 -0400

    Updates

--- angularapi/grunt-scripts/boomstrap-angular.js
+++ angularapi/grunt-scripts/boomstrap-angular.js
@@ -74,6 +74,40 @@
     }
   ]);
 }(angular.module('boomstrap')));
+(function (boomstrap) {
+  'use strict';
+  boomstrap.controller('CarouselDemoCtrl', [
+    '$scope',
+    function ($scope) {
+      $scope.myInterval = 5000;
+      var slides = $scope.slides = [];
+      $scope.addSlide = function () {
+        var newWidth = 600 + slides.length;
+        slides.push({
+          image: 'http://placekitten.com/' + newWidth + '/300',
+          text: [
+            'More',
+            'Extra',
+            'Lots of',
+            'Surplus'
+          ][slides.length % 4] + ' ' + [
+            'Cats',
+            'Kittys',
+            'Felines',
+            'Cutes'
+          ][slides.length % 4]
+        });
+      };
+      for (var i = 0; i < 4; i++) {
+        $scope.addSlide();
+      }
+      slides.push({
+        image: 'images/fpo-he-man.jpg',
+        text: 'Skeletor!?'
+      });
+    }
+  ]);
+}(angular.module('boomstrap')));
 (function (Boomstrap) {
   'use strict';
   /**
@@ -1335,40 +1369,6 @@
       return str.charAt(0).toUpperCase() + str.slice(1);
     };
   });
-}(angular.module('boomstrap')));
-(function (boomstrap) {
-  'use strict';
-  boomstrap.controller('CarouselDemoCtrl', [
-    '$scope',
-    function ($scope) {
-      $scope.myInterval = 5000;
-      var slides = $scope.slides = [];
-      $scope.addSlide = function () {
-        var newWidth = 600 + slides.length;
-        slides.push({
-          image: 'http://placekitten.com/' + newWidth + '/300',
-          text: [
-            'More',
-            'Extra',
-            'Lots of',
-            'Surplus'
-          ][slides.length % 4] + ' ' + [
-            'Cats',
-            'Kittys',
-            'Felines',
-            'Cutes'
-          ][slides.length % 4]
-        });
-      };
-      for (var i = 0; i < 4; i++) {
-        $scope.addSlide();
-      }
-      slides.push({
-        image: 'images/fpo-he-man.jpg',
-        text: 'Skeletor!?'
-      });
-    }
-  ]);
 }(angular.module('boomstrap')));
 (function (Boomstrap, Tour) {
   'use strict';
