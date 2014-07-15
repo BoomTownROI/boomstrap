e0ec36d9dfe3a5834b5ef99411e7662b39ebae3b
Sean McCambridge <sean@boomtownroi.com>
2014-07-15 17:14:46 -0400

    added bt-body-loader directive

---
+++ app/scripts/directives/btBodyLoader.js
@@ -0,0 +1,25 @@
+(function(Boomstrap) {
+  'use strict';
+  Boomstrap.directive('btBodyLoader', function($parse, $document) {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        isLoading: '='
+      },
+      link: function(scope, el, attrs) {
+        var body = $document.find('body'),
+            template = '<div id="bt-body-loader" class="loader body-loader"><span class="loader-pulse"></span><span class="loader-pulse"></span><span class="loader-pulse"></span></div>';
+
+        body.append(template);
+
+        scope.$watch('isLoading', function(newVal) {
+          var $loader = angular.element('#bt-body-loader'),
+              display = (newVal.value === false) ? 'none' : 'block';
+
+          $loader.css({'display': display});
+        }, true);
+      }
+    };
+  });
+})(angular.module('boomstrap'));
