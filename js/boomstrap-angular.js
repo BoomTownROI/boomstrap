b6d49a379efdfed8c13a6634d676014931ba66b7
Sean McCambridge <sean@boomtownroi.com>
2014-07-16 11:32:40 -0400

    Updates

--- js/boomstrap-angular.js
+++ js/boomstrap-angular.js
@@ -446,7 +446,11 @@
       replace: true,
       scope: { isLoading: '=' },
       link: function (scope) {
-        var body = $document.find('body'), template = '<div id="bt-body-loader" class="loader body-loader"><span class="loader-pulse"></span><span class="loader-pulse"></span><span class="loader-pulse"></span></div>';
+        var body = $document.find('body'), loader = $document.find('#bt-body-loader'), template = '<div id="bt-body-loader" class="loader body-loader"><span class="loader-pulse"></span><span class="loader-pulse"></span><span class="loader-pulse"></span></div>';
+        if (loader.length > 0) {
+          return true;
+        }
+        // there can be only one
         body.append(template);
         scope.$watch('isLoading', function (newVal) {
           var $loader = angular.element('#bt-body-loader'), display = newVal.value === false ? 'none' : 'block';
