afbb0edddc359939567d34345de2133f2c13d46c
Sean McCambridge <sean@boomtownroi.com>
2014-07-16 09:53:49 -0400

    Updates

--- angularapi/grunt-scripts/boomstrap-angular.js
+++ angularapi/grunt-scripts/boomstrap-angular.js
@@ -440,6 +440,24 @@
 }(angular.module('boomstrap')));
 (function (Boomstrap) {
   'use strict';
+  Boomstrap.directive('btBodyLoader', function ($parse, $document) {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: { isLoading: '=' },
+      link: function (scope) {
+        var body = $document.find('body'), template = '<div id="bt-body-loader" class="loader body-loader"><span class="loader-pulse"></span><span class="loader-pulse"></span><span class="loader-pulse"></span></div>';
+        body.append(template);
+        scope.$watch('isLoading', function (newVal) {
+          var $loader = angular.element('#bt-body-loader'), display = newVal.value === false ? 'none' : 'block';
+          $loader.css({ 'display': display });
+        }, true);
+      }
+    };
+  });
+}(angular.module('boomstrap')));
+(function (Boomstrap) {
+  'use strict';
   /**
    * @ngdoc directive
    * @name  boomstrap.directive:btCenterOnPage
