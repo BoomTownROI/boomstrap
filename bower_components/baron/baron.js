63aee6978ff154565e5acec88a97f748e4900b31
Ian Greulich <ian@greulich.me>
2014-09-03 13:49:56 -0400

    remove console.log

--- bower_components/baron/baron.js
+++ bower_components/baron/baron.js
@@ -1075,7 +1075,6 @@ var
         var self = this;
 
         this._observer = new MutationObserver(function() {
-            console.log('observe callback');
             self.update();
         });
 
