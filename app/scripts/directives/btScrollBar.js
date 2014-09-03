5d71fbe73aed59d1defef6d01a8a741f2bffd0dd
Ian Greulich <ian@greulich.me>
2014-09-03 12:25:24 -0400

    hide the baron scrollbar if the contents are smaller than the viewable area, show it if it is

--- app/scripts/directives/btScrollBar.js
+++ app/scripts/directives/btScrollBar.js
@@ -16,7 +16,8 @@
       replace: true,
       templateUrl: 'template/scrollbar/bt-scrollbar.tpl.html',
       link: function(scope, element) {
-        var $element = angular.element(element);
+        var $element  = angular.element(element);
+        var $scroller = $element.find('.baron-scroller')[0];
 
         var scroll = baron({
           root: element,
@@ -26,10 +27,18 @@
           $: angular.element
         });
         // Seriously hacky. Need to add a subdirective for elements in the scrollbar that need to trigger the scroller update
+        // And need to bind a more generic event. Here we only update on click.
         $element.on('click', function() {
           $window.setTimeout(function() {
             scroll.update();
           }, 400);
+          if ($scroller.scrollHeight <= $scroller.clientHeight) {
+            $element[0].classList.remove('baron');
+          } else {
+            if (!$element[0].classList.contains('baron')) {
+              $element[0].classList.add('baron');
+            }
+          }
         });
 
         scope.$on('$destroy', function() {
