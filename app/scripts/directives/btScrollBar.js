859cafb377a41f9c912881c65d6b848a3b499482
Craig Anthony <mcraiganthony@gmail.com>
2014-09-03 14:43:07 -0400

    Craig - revert btScrollBar directive

--- app/scripts/directives/btScrollBar.js
+++ app/scripts/directives/btScrollBar.js
@@ -16,9 +16,7 @@
       replace: true,
       templateUrl: 'template/scrollbar/bt-scrollbar.tpl.html',
       link: function(scope, element) {
-        var $element   = angular.element(element);
-        var $scroller  = $element.find('.baron-scroller')[0];
-        var $scrollbar = $element.find('.baron-scroller-bar')[0];
+        var $element = angular.element(element);
 
         var scroll = baron({
           root: element,
@@ -28,18 +26,9 @@
           $: angular.element
         });
         // Seriously hacky. Need to add a subdirective for elements in the scrollbar that need to trigger the scroller update
-        // And need to bind a more generic event. Here we only update on click.
         $element.on('click', function() {
           $window.setTimeout(function() {
             scroll.update();
-
-            if ($scroller.scrollHeight <= $scroller.clientHeight) {
-              $scrollbar.classList.add('hidden');
-            } else {
-              if ($scrollbar.classList.contains('hidden')) {
-                $scrollbar.classList.remove('hidden');
-              }
-            }
           }, 400);
         });
 
--- dist/js/boomstrap-angular.js
+++ dist/js/boomstrap-angular.js
@@ -1247,8 +1247,6 @@
       templateUrl: 'template/scrollbar/bt-scrollbar.tpl.html',
       link: function (scope, element) {
         var $element = angular.element(element);
-        var $scroller = $element.find('.baron-scroller')[0];
-        var $scrollbar = $element.find('.baron-scroller-bar')[0];
         var scroll = baron({
             root: element,
             scroller: '.baron-scroller',
@@ -1257,17 +1255,9 @@
             $: angular.element
           });
         // Seriously hacky. Need to add a subdirective for elements in the scrollbar that need to trigger the scroller update
-        // And need to bind a more generic event. Here we only update on click.
         $element.on('click', function () {
           $window.setTimeout(function () {
             scroll.update();
-            if ($scroller.scrollHeight <= $scroller.clientHeight) {
-              $scrollbar.classList.add('hidden');
-            } else {
-              if ($scrollbar.classList.contains('hidden')) {
-                $scrollbar.classList.remove('hidden');
-              }
-            }
           }, 400);
         });
         scope.$on('$destroy', function () {
