b4dacc83e59784a90b78050abf750dd2bd5eb9ac
Craig Anthony <mcraiganthony@gmail.com>
2014-12-09 15:44:28 -0500

    Craig - modal eqs

--- js/element-queries.js
+++ js/element-queries.js
@@ -1,26 +1,32 @@
-// Define element queries (BoomQuery - https://github.com/BoomTownROI/boomqueries)
+// Element Queries (BoomQueries - https://github.com/BoomTownROI/boomqueries)
+
+// Define element queries
+// -------------------------
 
 boomQueries.add(".texting", [[610, "texting--md"]]);
 boomQueries.add(".log-call", [[420, "log-call--sm"]]);
 boomQueries.add(".set-to-do", [[420, "set-to-do--sm"]]);
 
+// document.addEventListener('DOMContentLoaded', boomQueries.refresh, false);
+
 $(document).ready(function() {
   boomQueries.refresh();
 });
 
-// document.addEventListener('DOMContentLoaded', boomQueries.refresh, false);
+// Modal element queries refresh
+// -------------------------
 
 $( document ).ready(function() {
 
-// 	$('.modal').on('show.bs.modal', function (e) {
-// 		setTimeout(function(){
-// 			boomQueries.refresh();
-// 		}, 200);
-// 	});
+/*	$('.modal').on('show.bs.modal', function (e) {
+		setTimeout(function(){
+			boomQueries.refresh();
+		}, 200);
+	});*/
 
-$('.modal').on('shown.bs.modal', function (e) {
-	boomQueries.refresh();
+	$('.modal').on('shown.bs.modal', function (e) {
+		boomQueries.refresh();
+	});
+
 });
-
- });
 
