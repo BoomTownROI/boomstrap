99bbac048c808f69f0f9fe12040146e60f9e547e
Craig Anthony <mcraiganthony@gmail.com>
2014-12-11 11:56:37 -0500

    Updates

--- angularapi/grunt-scripts/boomstrap-angular.js
+++ angularapi/grunt-scripts/boomstrap-angular.js
@@ -1472,6 +1472,58 @@
     }
   ]);
 }(angular.module('boomstrap')));
+(function (Boomstrap) {
+  'use strict';
+  Boomstrap.filter('capitalize', function () {
+    return function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+  });
+}(angular.module('boomstrap')));
+(function (Boomstrap) {
+  'use strict';
+  /**
+   * @ngdoc filter
+   * @name phoneNumber
+   *
+   * @description The 'phoneNumberfilter' filter will format or unformat a phone number.
+   * {{string|phoneNumber:'add'}} to format the number, {{string|phoneNumber:'remove'}}.
+   *
+   * @example
+     $scope.string1 = '1234567890';
+     <br />
+     $scope.string2 = '123-456-7890';
+
+     {{string1|phoneNumber:'add'}} // outputs (123) 456-7890
+     <br />
+     {{string2|phoneNumber:'remove'}} // outputs 1234567890
+   */
+  Boomstrap.filter('phoneNumber', function () {
+    return function (string, params) {
+      var number = string || '';
+      var formattedNumber;
+      var localPrefix;
+      var localMain;
+      var area;
+      switch (params) {
+      case 'remove':
+        formattedNumber = number.replace(/\D/g, '');
+        break;
+      case 'add':
+        number = number.replace(/\D/g, '');
+        area = number.substring(0, 3);
+        localPrefix = number.substring(3, 6);
+        localMain = number.substring(6);
+        formattedNumber = '(' + area + ') ' + localPrefix + '-' + localMain;
+        break;
+      default:
+        formattedNumber = string;
+        break;
+      }
+      return formattedNumber;
+    };
+  });
+}(angular.module('boomstrap')));
 (function (Boomstrap, Tour) {
   'use strict';
   Boomstrap.service('bootstrapTourService', function ($templateCache, $rootScope, $http, AUTO_START_TOUR) {
@@ -1590,67 +1642,15 @@
     return tour;
   });
 }(angular.module('boomstrap'), window.Tour));
-(function (Boomstrap) {
-  'use strict';
-  Boomstrap.filter('capitalize', function () {
-    return function (str) {
-      return str.charAt(0).toUpperCase() + str.slice(1);
-    };
-  });
-}(angular.module('boomstrap')));
-(function (Boomstrap) {
-  'use strict';
-  /**
-   * @ngdoc filter
-   * @name phoneNumber
-   *
-   * @description The 'phoneNumberfilter' filter will format or unformat a phone number.
-   * {{string|phoneNumber:'add'}} to format the number, {{string|phoneNumber:'remove'}}.
-   *
-   * @example
-     $scope.string1 = '1234567890';
-     <br />
-     $scope.string2 = '123-456-7890';
-
-     {{string1|phoneNumber:'add'}} // outputs (123) 456-7890
-     <br />
-     {{string2|phoneNumber:'remove'}} // outputs 1234567890
-   */
-  Boomstrap.filter('phoneNumber', function () {
-    return function (string, params) {
-      var number = string || '';
-      var formattedNumber;
-      var localPrefix;
-      var localMain;
-      var area;
-      switch (params) {
-      case 'remove':
-        formattedNumber = number.replace(/\D/g, '');
-        break;
-      case 'add':
-        number = number.replace(/\D/g, '');
-        area = number.substring(0, 3);
-        localPrefix = number.substring(3, 6);
-        localMain = number.substring(6);
-        formattedNumber = '(' + area + ') ' + localPrefix + '-' + localMain;
-        break;
-      default:
-        formattedNumber = string;
-        break;
-      }
-      return formattedNumber;
-    };
-  });
-}(angular.module('boomstrap')));
 angular.module("ui.bootstrap").run(["$templateCache", function($templateCache) {$templateCache.put("template/pager/bt-pager.tpl.html","<div class=\"btn-group minimal-pager\">\n    <button\n        type=\"button\"\n        class=\"btn btn-default btn-icon\"\n        ng-class=\"{ \'disabled\': noPrevious() }\"\n        ng-click=\"selectPage(page - 1)\"><i\n            class=\"ficon ficon-chevron-left\"></i></button>\n    <button\n        type=\"button\"\n        class=\"btn btn-default btn-icon\"\n        ng-class=\"{ \'disabled\': noNext() }\"\n        ng-click=\"selectPage(page + 1)\"><i\n            class=\"ficon ficon-chevron-right\"></i></button>\n</div>");}]);
 angular.module("boomstrap").run(["$templateCache", function($templateCache) {$templateCache.put("template/nav.html","<nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">\n  <div class=\"container-fluid\">\n    <div class=\"navbar-header\">\n      <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#pl-nav\">\n        <span class=\"icon-bar\"></span>\n        <span class=\"icon-bar\"></span>\n        <span class=\"icon-bar\"></span>\n      </button>\n      <a class=\"navbar-brand\" href=\"#\">PL</a>\n    </div>\n    <div class=\"collapse navbar-collapse\" id=\"pl-nav\">\n      <ul class=\"nav navbar-nav\">\n        <li class=\"active\"><a href=\"#pl-colors\">Colors</a></li>\n        <li class=\"dropdown\">\n          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">Buttons <b class=\"caret\"></b></a>\n          <ul class=\"dropdown-menu\">\n            <li><a href=\"#pl-button-options\">Options</a></li>\n            <li><a href=\"#pl-button-sizes\">Sizes</a></li>\n            <li><a href=\"#pl-button-active\">Active State</a></li>\n            <li><a href=\"#pl-button-disabled\">Disabled State</a></li>\n            <li><a href=\"#pl-button-tags\">Button Tags</a></li>\n          </ul>\n        </li>\n        <li><a href=\"#pl-labels\">Labels</a></li>\n        <li><a href=\"#pl-typography\">Typography</a></li>\n      </ul>\n    </div>\n  </div>\n</nav>\n<div class=\"container\">");
 $templateCache.put("template/btLazyPen/btLazyPen.tpl.html","<div class=\"bt-lazy-pen\">\n  <span class=\"btn btn-attention\" ng-if=\"!showingPen.value\" ng-click=\"showingPen.value = !showingPen.value\">Load CodePen Example</span>\n  <div ng-if=\"showingPen.value\">\n    <p data-height=\"{{ height }}\" data-theme-id=\"{{ themeId }}\" data-slug-hash=\"{{ slug }}\" data-default-tab=\"result\" class=\'codepen\'>See the Pen <a href=\'http://codepen.io/{{ user }}/pen/{{ slug }}/\'>{{ title }}</a> by {{ author }} (<a href=\'http://codepen.io/{{ user }}\'>@{{ userId }}</a>) on <a href=\'http://codepen.io\'>CodePen</a>.</p>\n    <script async src=\"//codepen.io/assets/embed/ei.js\"></script>\n  </div>\n</div>");
 $templateCache.put("template/btPager/bt-pager.tpl.html","<pager\n	template-url=\"template/pager/bt-pager.tpl.html\"\n	total-items=\"totalItems\"\n	items-per-page=\"itemsPerPage\"\n	ng-model=\"currentPage\">\n</pager>");
 $templateCache.put("template/carousel/carousel.html","<div ng-mouseenter=\"pause()\" ng-mouseleave=\"play()\" class=\"carousel\">\n    <ol class=\"carousel-indicators\" ng-show=\"slides.length > 1\">\n        <li ng-repeat=\"slide in slides\" ng-class=\"{active: isActive(slide)}\" ng-click=\"select(slide)\"></li>\n    </ol>\n    <div class=\"carousel-inner\" ng-transclude></div>\n    <a class=\"left carousel-control\" ng-click=\"prev()\" ng-show=\"slides.length > 1\"><span class=\"ficon ficon-chevron-left\"></span></a>\n    <a class=\"right carousel-control\" ng-click=\"next()\" ng-show=\"slides.length > 1\"><span class=\"ficon ficon-chevron-right\"></span></a>\n</div>");
 $templateCache.put("template/dropdown/bt-dropdown.tpl.html","<div class=\"dropdown\">\n    <button class=\"btn btn-default dropdown-toggle\" type=\"button\">\n        <span class=\"pull-left\" ng-bind=\"selectedValue\"></span>\n        <span class=\"caret\"></span>\n        <!-- <i class=\"ficon ficon-chevron-down pull-right\"></i> -->\n    </button>\n    <ul class=\"dropdown-menu\" role=\"menu\" ng-style=\"{ \'min-width\': dropdownWidth + \'px\'}\">\n        <li ng-repeat=\"value in arrayValues\" ng-if=\"keysAreNumbers\">\n            <a ng-click=\"assignValue(value.key)\">{{ value.value }}</a>\n        </li>\n        <li ng-repeat=\"(choiceValue, choiceName) in values\" ng-if=\"!keysAreNumbers\">\n            <a ng-click=\"assignValue(choiceValue)\">{{choiceName}}</a>\n        </li>\n    </ul>\n</div>");
+$templateCache.put("template/loader/bt-loader.tpl.html","<div class=\"loader\">\n  <span class=\"loader-pulse\"></span>\n  <span class=\"loader-pulse\"></span>\n  <span class=\"loader-pulse\"></span>\n</div>");
 $templateCache.put("template/popover/popover-bootstrap-tour.html","<div class=\"popover\">\n    <div class=\"arrow\"></div>\n    <div class=\"popover-close\">\n        <i data-role=\'end\' class=\"ficon ficon-cross property-close\"></i>\n    </div>\n    <h3 class=\"popover-title\">Tour</h3>\n    <div class=\"tour-popover popover-content\"></div>\n    <div class=\"popover-navigation\">\n        <button class=\"btn btn-sm btn-default\" data-role=\"prev\">Prev</button>\n        <button class=\"btn btn-sm btn-primary\" data-role=\"next\"><span>Next</span></button>\n    </div>\n</div>");
 $templateCache.put("template/popover/template-popover.html","<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n  <div class=\"arrow\"></div>\n\n  <div class=\"popover-close\" ng-click=\"$popover.close($event)\" aria-hidden=\"true\"><i class=\"ficon ficon-cross\"></i></div>\n\n  <h3 class=\"popover-title\" ng-bind=\"title\" ng-show=\"title\"></h3>\n\n  <div class=\"popover-content\" ng-include=\"content\"></div>\n</div>");
-$templateCache.put("template/loader/bt-loader.tpl.html","<div class=\"loader\">\n  <span class=\"loader-pulse\"></span>\n  <span class=\"loader-pulse\"></span>\n  <span class=\"loader-pulse\"></span>\n</div>");
 $templateCache.put("template/property-card/bt-property-card-sm.tpl.html","<div class=\"card card-sm\">\n  <!-- ///////////////////////////////////////////////// -->\n  <!-- Optional: replace \"card-photo\" div with carousel -->\n  <!-- ///////////////////////////////////////////////// -->\n  <div class=\"card-photo\">\n    <div class=\"card-photo-inner\">\n      <div ng-if=\"property.newProperty\" class=\"sash sash-new\">New <span class=\"sash-time\" am-time-ago=\"{{ property.newProperty }}\"></span></div>\n      <div ng-if=\"property.offMarket\" class=\"sash sash-off\">Off Market <span class=\"sash-time\" am-time-ago=\"{{ property.offMarket }}\"></span></div>\n      <div ng-if=\"property.reduced\" class=\"sash sash-reduced\"><i class=\"ficon ficon-arrow-down\"></i> {{ property.reduced.change }} ({{ property.reduced.changePercent }}) <span class=\"sash-time\" am-time-ago=\"{{ property.reduced.when }}\"></span></div>\n      <div ng-if=\"property.backOnMarket\" class=\"sash sash-back\">Back <span class=\"sash-time\" am-time-ago=\"{{ property.backOnMarket }}\"></span></div>\n      <img ng-if=\"property.imageSrc.length <= 1\" bt-error-img=\"http://boomstatic.com/images/comingsoon-lg.jpg\" class=\"card-img\" ng-src=\"{{ property.imageSrc[0] }}\" alt=\"{{ property.fullAddress }}\">\n      <carousel ng-if=\"property.imageSrc.length > 1\">\n        <slide ng-repeat=\"slide in property.imageSrc\">\n          <img ng-src=\"{{ slide }}\" style=\"margin: auto\">\n        </slide>\n      </carousel>\n    </div>\n  </div>\n  <!-- ///////////////////////////////////////////////// -->\n  <div class=\"card-sm-container\">\n     <div class=\"row row-xcondensed\">\n      <div class=\"col-xs-8\">\n        <p class=\"card-sm-priority card-sm-street\">\n          <a target=\"_blank\" href=\"{{ property.listingUrl }}\">{{ property.address.street }}</a>\n        </p>\n        <p class=\"xsmall\">{{ property.address.city }}, {{ property.address.state }}</p>\n        <p class=\"xsmall\">{{ property.address.neighborhood }}</p>\n      </div>\n      <div class=\"col-xs-4 text-right\">\n        <p class=\"card-sm-priority card-sm-price\">{{ property.listPrice }}</p>\n        <p class=\"xsmall\">{{ property.pricePerSqft }}/SQFT</p>\n      </div>\n     </div>\n  </div>\n  <div class=\"card-sm-stats\">\n    <span class=\"card-sm-stat\">{{ property.beds }} BEDS</span>\n    <span class=\"card-sm-stat\">{{ property.baths }} BATHS</span>\n    <span class=\"card-sm-stat\">{{ property.sqft }} SQFT</span>\n    <span class=\"card-sm-stat\">{{ property.acres }} ACRES</span>\n  </div>\n  <div class=\"card-sm-container\">\n    <div class=\"row row-xcondensed\">\n      <div class=\"col-sm-6\">\n        <button type=\"button\" class=\"btn btn-default btn-block btn-sm\" ng-click=\"onClickBestFit($event)\"><i class=\"ficon ficon-star\"></i> {{ property.bestFits }} Best-Fit</button>\n      </div>\n      <div class=\"col-sm-6\">\n        <button type=\"button\" class=\"btn btn-default btn-block btn-sm\" ng-click=\"onClickFavs($event)\"><i class=\"ficon ficon-heart\"></i> {{ property.favs }} Favs</button>\n      </div>\n    </div>\n  </div> <!-- /card-container -->\n</div>");
 $templateCache.put("template/property-card/bt-property-card.tpl.html","<div class=\"card\" ng-class=\"{ \'card-sm\': size === \'sm\' }\">\n  <div ng-if=\"property.newProperty\" class=\"sash sash-new\">New <span class=\"sash-time\" am-time-ago=\"{{ property.newProperty }}\"></span></div>\n  <div ng-if=\"property.offMarket\" class=\"sash sash-off\">Off Market <span class=\"sash-time\" am-time-ago=\"{{ property.offMarket }}\"></span></div>\n  <div ng-if=\"property.reduced\" class=\"sash sash-reduced\"><i class=\"ficon ficon-arrow-down\"></i> {{ property.reduced.change }} ({{ property.reduced.changePercent }}) <span class=\"sash-time\" am-time-ago=\"{{ property.reduced.when }}\">{{ property.reduced.when }}</span></div>\n  <div ng-if=\"property.backOnMarket\" class=\"sash sash-back\">Back <span class=\"sash-time\" am-time-ago=\"{{ property.backOnMarket }}\">{{ property.backOnMarket }}</span></div>\n  <div class=\"card-photo\">\n    <div class=\"card-photo-inner\">\n      <img ng-if=\"property.imageSrc.length <= 1\" bt-error-img=\"http://boomstatic.com/images/comingsoon-lg.jpg\" class=\"card-img\" ng-src=\"{{ property.imageSrc[0] }}\" alt=\"{{ property.fullAddress }}\">\n      <carousel ng-if=\"property.imageSrc.length > 1\">\n        <slide ng-repeat=\"slide in property.imageSrc\">\n          <img ng-src=\"{{ slide }}\" style=\"margin: auto\">\n        </slide>\n      </carousel>\n    </div>\n  </div>\n  <div class=\"card-container\">\n     <div class=\"row row-xcondensed\">\n        <p class=\"card-priority card-price\">{{ property.listPrice }}</p>\n        <p class=\"card-priority card-street\">\n          <a target=\"_blank\" href=\"{{ property.listingUrl }}\">{{ property.address.street }}</a>\n        </p>\n      <div class=\"col-xs-7\">\n        <p class=\"small\">{{ property.address.city }}, {{ property.address.state }}</p>\n        <p class=\"small\">{{ property.address.neighborhood }}</p>\n      </div>\n      <div class=\"col-xs-5 text-right\">\n        <p class=\"small\">{{ property.pricePerSqft }}/SQFT</p>\n      </div>\n     </div>\n  </div>\n  <div class=\"card-stats\">\n    <span class=\"card-stat\">{{ property.beds }} BEDS</span>\n    <span class=\"card-stat\">{{ property.baths }} BATHS</span>\n    <span class=\"card-stat\">{{ property.sqft }} SQFT</span>\n    <span class=\"card-stat\">{{ property.acres }} ACRES</span>\n  </div>\n  <div class=\"card-container\">\n    <div class=\"row row-xcondensed\">\n      <div class=\"col-sm-6\">\n        <button type=\"button\" class=\"btn btn-default btn-block btn-sm\" ng-click=\"onClickBestFit($event)\"><i class=\"ficon ficon-star\"></i> {{ property.bestFits }} Best-Fit</button>\n      </div>\n      <div class=\"col-sm-6\">\n        <button type=\"button\" class=\"btn btn-default btn-block btn-sm\" ng-click=\"onClickFavs($event)\"><i class=\"ficon ficon-heart\"></i> {{ property.favs }} Favs</button>\n      </div>\n    </div>\n  </div> <!-- /card-container -->\n  <div ng-if=\"!isSmall\">\n    <div class=\"card-toggle\">\n        <span ng-click=\"isExpanded = !isExpanded\" ng-show=\"!isExpanded\"><i class=\"ficon ficon-chevron-down\"></i> More Detail</span>\n        <span ng-click=\"isExpanded = !isExpanded\" ng-show=\"isExpanded\"><i class=\"ficon ficon-chevron-up\"></i> Less Detail</span>\n    </div>\n    <div collapse=\"!isExpanded\" class=\"card-detail\">\n      <div class=\"card-container\">\n        <div class=\"row row-xcondensed\">\n          <div class=\"col-xs-6\">\n            <p class=\"small\"><strong>MLS#:</strong> <a target=\"_blank\" href=\"\">{{ property.mls }}</a></p>\n          </div>\n          <div class=\"col-xs-6 text-right\">\n            <p class=\"small\"><strong>LISTED:</strong> {{ property.listed }}</p>\n          </div>\n        </div>\n        <p class=\"small\"><strong>TYPE:</strong> {{ property.type }}</p>\n      </div> <!-- /card-container -->\n      <p class=\"card-title\">Price History</p>\n      <div class=\"card-container card-price-history\">\n        <div ng-repeat=\"history in property.history\" class=\"row row-xcondensed\">\n          <div class=\"col-xs-3\">\n            <p class=\"small\">{{ history.when }}</p>\n          </div>\n          <div class=\"col-xs-5 col-xs-offset-1\">\n            <p class=\"small card-price-history-change\"><i class=\"ficon ficon-arrow-down\"></i> {{ history.change }} ({{ history.changePercent }})</p>\n          </div>\n          <div class=\"col-xs-3 text-right\">\n            <p class=\"small\">{{ history.price }}</p>\n          </div>\n        </div>\n      </div> <!-- /card-price-history -->\n      <div class=\"card-container\">\n        <div class=\"row row-xcondensed\">\n          <div class=\"col-sm-6\">\n            <button class=\"btn btn-default btn-sm btn-block\"><i class=\"ficon ficon-location\"></i> Map View</button>\n          </div>\n          <div class=\"col-sm-6\">\n            <button class=\"btn btn-default btn-sm btn-block\">Full Details <i class=\"ficon ficon-arrow-bend-right\"></i> </button>\n          </div>\n        </div>\n      </div> <!-- /card-container -->\n    </div> <!-- /card-detail -->\n  </div>\n</div>");
 $templateCache.put("template/scrollbar/bt-scrollbar.tpl.html","<div class=\"baron\">\n  <div class=\"baron-scroller\" ng-transclude></div>\n  <div class=\"baron-scroller-track\">\n    <div class=\"baron-scroller-bar\"></div>\n  </div>\n</div>\n");
