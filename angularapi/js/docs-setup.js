c99306a4db5bbd8ae8e37b841b5f7f00280e4f43
Craig Anthony <mcraiganthony@gmail.com>
2015-01-06 13:32:18 -0500

    Updates

--- angularapi/js/docs-setup.js
+++ angularapi/js/docs-setup.js
@@ -5,24 +5,6 @@ NG_DOCS={
   "pages": [
     {
       "section": "ngboomstrap",
-      "id": "boomstrap.directive:btAddClassOnLoad",
-      "shortName": "btAddClassOnLoad",
-      "type": "directive",
-      "moduleName": "boomstrap",
-      "shortDescription": "The btAddClassOnLoad directive adds a class to an img tag",
-      "keywords": "add adds boomstrap bt-add-class-on-load btaddclassonload class css directive event fires image images img jpg linear load loaded module my-img ngboomstrap opacity representation src string tag text transition transitions type"
-    },
-    {
-      "section": "ngboomstrap",
-      "id": "boomstrap.directive:btAffix",
-      "shortName": "btAffix",
-      "type": "directive",
-      "moduleName": "boomstrap",
-      "shortDescription": "The btAffix element allows a user to affix an element at a given scroll point.",
-      "keywords": "$window add affix affixed affixing allowed allows boomstrap break bt-affix btaffix directive element ensure entire fullheight height images indicates jpg layout left module ngboomstrap number offset pinnedheader pixels place point scroll spans src top user width"
-    },
-    {
-      "section": "ngboomstrap",
       "id": "boomstrap.directive:btArrowScroll",
       "shortName": "btArrowScroll",
       "type": "directive",
