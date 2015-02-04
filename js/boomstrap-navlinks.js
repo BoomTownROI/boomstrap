ea82ad69dfaba1a8ba5d011ecdd535675f02b34b
Sean McCambridge <sean@boomtownroi.com>
2015-02-04 11:17:27 -0500

    refactored navlinks plugin

--- js/boomstrap-navlinks.js
+++ js/boomstrap-navlinks.js
@@ -6,75 +6,53 @@ Boomstrap Navigation Links and Navigation Link Blocks
 (function($) {
   $.fn.btNavLinks = function() {
 
+    // render and transform marker line
     var handleNavLinksBar = function($navLinks) {
-      var $nav = $navLinks || $(this);
-      if (!$nav.hasClass('nav-links-initialized')) {
-        $nav.addClass('nav-links-initialized');
+      //  passed by the click event or fire on init
+      var $nav = $(this);
+      if (!$nav.hasClass('nav-links-init')) { // not inited yet
+        $nav.addClass('nav-links-init');
         $nav.append('<span class="nav-links__bar"></span>');
       }
       var activeLink = $nav.find('li.active');
       var navLinksBar = $nav.find('.nav-links__bar');
-      navLinksBar.css({
-        transform: 'translateX(' + activeLink.position().left + 'px)',
-        width: activeLink.width()
-      });
-    }
-
-    // Handle click behavior
-    var handleNavLinksClick = function(e) {
-      e.preventDefault();
-      var $navLinks = $(this).closest($('.nav-links'));
-      $navLinks.find('li').removeClass('active');
-      var activeLink = $(e.target).closest($('li'));
-      activeLink.addClass('active');
-      handleNavLinksBar.call(this, $navLinks);
-    };
-
-    this.each(function() {
-      // make adjustments to nav links bar
-      handleNavLinksBar.apply(this);
-    });
-
-    $(document).on('click', '.nav-links > li > a', handleNavLinksClick);
-
-    return this;
-
-  };
-})(jQuery);
-
-(function($) {
-  $.fn.btNavBlocks = function() {
-
-    var handleNavLinksBar = function($navLinks) {
-      var $nav = $navLinks || $(this);
-      if (!$nav.hasClass('nav-blocks-initialized')) {
-        $nav.addClass('nav-blocks-initialized');
-        $nav.append('<span class="nav-blocks__bar"></span>');
+      
+      // render bar
+      if ($nav.hasClass('nav-links')) { // horizontal
+        navLinksBar.css({
+          transform: 'translateX(' + activeLink.position().left + 'px)',
+          width: activeLink.width()
+        });
+      } else if ($nav.hasClass('nav-blocks')) { // vertical
+        navLinksBar.css({
+          transform: 'translateY(' + activeLink.position().top + 'px)',
+          height: activeLink.height()
+        });
       }
-      var activeLink = $nav.find('li.active');
-      var navLinksBar = $nav.find('.nav-blocks__bar');
-      navLinksBar.css({
-        transform: 'translateY(' + activeLink.position().top + 'px)',
-        height: activeLink.height()
-      });
     }
 
-    // Handle click behavior
+    // click handler
     var handleNavLinksClick = function(e) {
       e.preventDefault();
-      var $navLinks = $(this).closest($('.nav-blocks'));
+      if (!$(e.target).is('a')) {
+        return; // only allow clicks on 'a' tag (prevents clicks on extra space in nav from erroring)
+      }
+      // climb up to nav level
+      var $navLinks = $(this).closest($('.nav-links-init'));
+      // kill old active
       $navLinks.find('li').removeClass('active');
+      // add new active
       var activeLink = $(e.target).closest($('li'));
       activeLink.addClass('active');
+      // re-render bar
       handleNavLinksBar.call(this, $navLinks);
     };
 
-    this.each(function() {
-      // make adjustments to nav links bar
-      handleNavLinksBar.apply(this);
-    });
+    // initialize
+    handleNavLinksBar.apply(this);
 
-    $(document).on('click', '.nav-blocks-initialized > li > a', handleNavLinksClick);
+    // set up event
+    this.on('click', handleNavLinksClick);
 
     return this;
 
