330610bd20ce0541b236b1a7f197868593a46b9e
Craig Anthony <mcraiganthony@gmail.com>
2015-02-09 10:55:29 -0500

    Craig - clean up bq defs

--- dist/js/boomstrap.js
+++ dist/js/boomstrap.js
@@ -4255,26 +4255,19 @@ boomQueries.add(".texting", [[610, "texting--md"]]);
 boomQueries.add(".log-call", [[420, "log-call--sm"]]);
 boomQueries.add(".set-to-do", [[420, "set-to-do--sm"]]);
 
-// document.addEventListener('DOMContentLoaded', boomQueries.refresh, false);
-
 $(document).ready(function() {
   boomQueries.refresh();
-});
 
-// Modal element queries refresh
-// -------------------------
-
-$( document ).ready(function() {
-
-/*	$('.modal').on('show.bs.modal', function (e) {
+  // Modal element queries refresh
+  /*$('.modal').on('show.bs.modal', function (e) {
 		setTimeout(function(){
 			boomQueries.refresh();
 		}, 200);
 	});*/
-
-	$('.modal').on('shown.bs.modal', function (e) {
+  $('.modal').on('shown.bs.modal', function (e) {
 		boomQueries.refresh();
 	});
 
 });
+
 
