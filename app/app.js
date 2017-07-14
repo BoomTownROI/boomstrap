30cf0d4e35374a0800b4d14471ace555256b3893
Travis Cooper <travis@boomtownroi.com>
2017-07-13 20:39:37 -0400

    CI-France: Remove offending url change listeners

--- app/app.js
+++ app/app.js
@@ -11,7 +11,16 @@
   })
   .config(function(uiSelectConfig) {
     uiSelectConfig.theme = 'bootstrap';
-  });
+  })
+  .config( ['$provide', function ($provide){
+    // URL change listeners interact with React/SPA when doing pushState url changes
+    // So we need to remove the offending methods here
+    $provide.decorator('$browser', ['$delegate', function ($delegate) {
+      $delegate.onUrlChange = function () {};
+      $delegate.url = function () { return ""};
+      return $delegate;
+    }]);
+  }]);
 
   angular.module('ui.bootstrap')
   .config(function($provide) {
