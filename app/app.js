444a1cec2968857c73c2434db1b3831db143f0cb
Travis Cooper <travis@boomtownroi.com>
2017-07-18 11:31:42 -0400

    Update app.js to apply routing fix only if on the SPA.

--- app/app.js
+++ app/app.js
@@ -13,8 +13,8 @@
     uiSelectConfig.theme = 'bootstrap';
   })
   .config( ['$provide', function ($provide){
-    // URL change listeners interact with React/SPA when doing pushState url changes
-    // So we need to remove the offending methods here, iff we're on the SPA page
+    // URL change listeners interact with React/SPA when doing pushState url changes.
+    // So we need to remove the offending methods here, iff we're on the SPA page.
     var href = window.location.href || '';
     var isSPA = href.indexOf('/app/') >= 0;
     if (isSPA) {
